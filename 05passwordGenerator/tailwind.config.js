c0d325cb247f6ec45913f2f4065eb83f1e9b2f13
yashj29 <29yashjain29@gmail.com>
2024-08-13 01:56:02 +0530

    Project

---
+++ 05passwordGenerator/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    './node_modules/shadcn/ui/dist/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
