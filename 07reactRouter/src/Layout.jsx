da7f2d8fd45f3f9a4a25f5e1ba6cc0cf17e1e29b
yashj29 <29yashjain29@gmail.com>
2024-08-23 14:58:08 +0530

    React router crash course

---
+++ 07reactRouter/src/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
+
+export default function Layout(props) {
+    
+
+    return (
+        <>
+        <Header />
+        <Outlet />
+        <Footer />
+            
+        </>
+    )
+}
