03668ad47f312d316ddce593b6558c364a0e668c
yashj29 <29yashjain29@gmail.com>
2024-08-29 17:25:56 +0530

    Use of Context API

---
+++ 08miniContext/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React, {useContext} from 'react'
+import UserContext from '../context/UserContext'
+
+export function Profile(props) {
+    const {user} = useContext(UserContext)
+
+    if(!user) return <div>please login</div>
+
+    return <div>Welcome {user.username}</div>
+    
+                            
+}
