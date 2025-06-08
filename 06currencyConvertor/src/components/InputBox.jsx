e8c91f9100b2d79d7c7e12cb52d4a6af72473776
yashj29 <29yashjain29@gmail.com>
2025-06-08 23:12:38 +0530

    some changes

--- 06currencyConvertor/src/components/InputBox.jsx
+++ 06currencyConvertor/src/components/InputBox.jsx
@@ -33,7 +33,7 @@ function InputBox({
             <div className="w-1/2 flex flex-wrap justify-end text-right">
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
-                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    className="rounded-md px-1 py-0.5 h-10 text-sm bg-gray-100 cursor-pointer outline-none"
                     value={selectCurrency}
                     onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
                     disabled={currencyDisable}     
